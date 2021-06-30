4b2c381024db89e90b63e7c1aa70d46531d5c84f
haeni0117 <zion0117@ewhain.net>
2021-06-30 14:41:24 +0900

    20210630WED

--- BOJ 문제풀이/1946신입사원.py
+++ BOJ 문제풀이/1946신입사원.py
@@ -1,13 +1,14 @@
-# 모든 면에서 떨어지면 그 사람은 선발하지 않는다 -> 규칙
-# 서류 심사 & 면접
 import sys
-num_of_test = int(sys.stdin.readline())
-for k in range(num_of_test):
-    num_of_people = int(sys.stdin.readline())
-    _list = [list(sys.stdin.readline()) for i in range(num_of_people)]
-    for paper in _list: # 서류심사끼리 비교하는 방법? 모르겠다...
-        for interview is _list:
-            _list[paper][0] >
-            #지금 paper랑 interview랑 비교하는게아닌데?
-            
-
+input= sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    _list = sorted([list(map(int,input().strip().split())) for x in range(n)],key = lambda x : x[0])#input을 list로 받고, 그 리스트들을 요소로 하는 하나의 큰 리스트로 묶어준다. 
+    #서류심사를 기준으로 순위대로 해준다.
+    standard = _list[0][1]
+    cnt = 1 #cnt변수 까먹음 -> 서류평가 1등은 이미 뽑히는 것이 확정이기 떄문에 1이다.
+    for i in range(1,n):
+        if _list[i][1] < standard :
+            standard = _list[i][1]
+            cnt +=1    
+    print(cnt)
